761622d1aae0a0b5eadd7e545e507d9393a18108
Yan Yamim <yanyamim@gmail.com>
2023-10-25 21:27:31 -0300

    API

---
+++ aluno_ciclo.py
@@ -0,0 +1,97 @@
+import json
+
+# Lista para armazenar os alunos
+alunos = []
+
+ciclos_entry_list = []
+pesos_ciclos = {}
+id_turma = ""  # Variável para armazenar o ID da turma
+
+# Função para adicionar ciclos
+def adicionar_ciclo():
+    global id_turma  # Utilize a variável global id_turma
+    ciclo = input("Digite o nome do ciclo: ")
+    if ciclo:
+        id_turma = input("Digite o ID da turma: ")  # Defina o id_turma
+        peso_ciclo = float(input(f"Digite o peso para o ciclo '{ciclo}': "))
+        data_inicio = input(f"Quando começará o ciclo '{ciclo}'? ")
+        data_final = input(f"Quando terminará o ciclo '{ciclo}'? ")
+        pesos_ciclos[ciclo] = peso_ciclo
+        ciclos_entry_list.append({
+            'ciclo': ciclo,
+            'data_inicio': data_inicio,
+            'data_final': data_final,
+        })
+
+# Função para coletar as notas dos alunos e calcular a média ponderada
+def coletar_notas():
+    notas_aluno = []
+
+    for ciclo in ciclos_entry_list:
+        nota_ciclo = float(input(f"Digite a nota para o ciclo '{ciclo['ciclo']}': "))
+        notas_aluno.append({
+            'Ciclo': ciclo['ciclo'],
+            'Nota': nota_ciclo,
+            'Data de Início': ciclo['data_inicio'],
+            'Data de Término': ciclo['data_final'],
+            'ID da Turma': id_turma  # Utilize o id_turma definido na função adicionar_ciclo
+        })
+
+    # Calcular a média ponderada
+    soma_notas = sum(nota['Nota'] * pesos_ciclos[nota['Ciclo']] for nota in notas_aluno)
+    soma_pesos = sum(pesos_ciclos[nota['Ciclo']] for nota in notas_aluno)
+    if soma_pesos == 0:
+        media_ponderada = 0.0
+    else:
+        media_ponderada = soma_notas / soma_pesos
+
+    nome_aluno = input("Digite o nome do aluno: ")
+
+    aluno = {
+        'Nome': nome_aluno,
+        'id_turma': id_turma,
+        'Notas': notas_aluno,
+        'Média Ponderada': media_ponderada
+    }
+
+    alunos.append(aluno)
+
+    print("Notas coletadas com sucesso!")
+
+# Função para exibir informações dos alunos
+def exibir_notas():
+    for aluno in alunos:
+        print(f"Nome: {aluno['Nome']}")
+        print(f"ID da Turma: {aluno['id_turma']},")
+        for nota in aluno['Notas']:
+            print(f"Ciclo: {nota['Ciclo']}, Nota: {nota['Nota']}")
+            print(f"Data de Início: {nota['Data de Início']}, Data de Término: {nota['Data de Término']}")
+        print(f"Média Ponderada: {aluno['Média Ponderada']}\n")
+
+while True:
+    print("\nMenu:")
+    print("1. Adicionar Ciclo")
+    print("2. Coletar Notas")
+    print("3. Exibir Notas")
+    print("4. Sair e criar Json")
+
+    escolha = input("Escolha uma opção: ")
+
+    if escolha == '1':
+        adicionar_ciclo()
+    elif escolha == '2':
+        coletar_notas()
+    elif escolha == '3':
+        exibir_notas()
+    elif escolha == '4':
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
+
+if alunos:
+    # Ao sair do loop, crie o arquivo JSON com as notas
+    with open('notas_alunos.json', 'w') as json_file:
+        json.dump(alunos, json_file, indent=4)
+    print("Arquivo JSON criado com as notas dos alunos: 'notas_alunos.json'")
+else:
+    print("Nenhum dado foi registrado. Saindo do programa.")
---
+++ cadastro_turma.py
@@ -0,0 +1,75 @@
+import json
+
+# Carregar os dados existentes do arquivo JSON (se existir)
+try:
+    with open('alunos.json', 'r') as arquivo_json:
+        alunos = json.load(arquivo_json)
+except FileNotFoundError:
+    alunos = []
+
+# Contador das RAs
+contador_ra = 1
+
+# Lista para armazenar as turmas
+turmas = []
+
+while True:
+    id_turma = input("Digite o ID da turma: ")
+    data_inicio = input("Digite a data de início da turma: ")
+    data_final = input("Digite a data de término da turma: ")
+
+    # Lista para armazenar os alunos da turma
+    alunos_turma = []
+
+    while True:
+        nome_aluno = input(f"Digite o nome do aluno (Turma {id_turma}): ")
+
+        # Criar a RA como uma string
+        ra = str(contador_ra)
+
+        # Criar um dicionário com o nome, o ID da turma e a RA do aluno
+        aluno = {
+            'nome': nome_aluno,
+            'id_turma': id_turma,
+            'ra': ra
+        }
+
+        # Adicionar o aluno à lista de alunos da turma
+        alunos_turma.append(aluno)
+
+        # Incrementar o número da RA
+        contador_ra += 1
+
+        continuar = input("Deseja adicionar mais um aluno(a)? (S/N)").strip().lower()
+        if continuar != 's':
+            break
+
+    # Criar um dicionário com as informações da turma
+    turma = {
+        'id_turma': id_turma,
+        'data_inicio': data_inicio,
+        'data_final': data_final,
+        'alunos': alunos_turma
+    }
+
+    # Adicionar a turma à lista de turmas
+    turmas.append(turma)
+
+    continuar = input("Deseja criar outra turma? (S/N)").strip().lower()
+    if continuar != 's':
+        break
+
+# Exibir as informações das turmas
+print("\nInformações das Turmas:")
+for turma in turmas:
+    print(f"ID da Turma: {turma['id_turma']}")
+    print(f"Data de Início: {turma['data_inicio']}")
+    print(f"Data de Término: {turma['data_final']}")
+    print("Alunos:")
+    for aluno in turma['alunos']:
+        print(f"- Nome: {aluno['nome']}, ID da Turma: {aluno['id_turma']}, RA: {aluno['ra']}")
+    print("\n")
+
+# Exemplo de como salvar os dados em JSON
+with open('turmas.json', 'w') as arquivo_json:
+    json.dump(turmas, arquivo_json, indent=4)
