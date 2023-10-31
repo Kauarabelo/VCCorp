d7537c14612417ee012f4b481656745fff5fbf26
Yan Yamim <yanyamim@gmail.com>
2023-10-30 22:06:25 -0300

    API

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cadastro_turma
+import cadastrar_aluno
 import aluno_ciclo
 import editar_turmas
 import editar_aluno
@@ -6,8 +6,8 @@ import editar_aluno
 # Função para exibir opções de menu
 def exibir_menu():
     print("O que você quer fazer hoje?")
-    print("1. Cadastro de turmas e Ciclos de Notas")
-    print("2. Editar alunos")
+    print("1. Cadastros")
+    print("2. Editar")
     print("3. Relatório")
     print("4. Sair")
 
@@ -17,25 +17,33 @@ while True:
     opcao = input("Opção: ")
 
     if opcao == "1":
-        escolha_1 = str(input("A. Cadastro de turmas / B. Ciclos de Notas: ")).strip().lower()
+        escolha_1 = str(input("A. Cadastro de aluno / B. Cadastro de Turmas / C. Grupos de Aluno / D. Ciclos/Notas: ")).strip().lower()
         if escolha_1 == 'a':
-            cadastro_turma.cadastrar_turma()
-        elif escolha_1 == 'b':
+            cadastrar_aluno.cadastrar_alunos()
+
+        #elif escolha_1 == 'b'
+        #elif escolha_1 == 'c'
+
+        elif escolha_1 == 'd':
             aluno_ciclo.registrar_ciclos_e_notas()
 
     elif opcao == "2":
-        escolha_2 = str(input("A. Editar Aluno / B. Editar Turma: ")).strip().lower()
-        if escolha_2 == 'a':  # Corrigido de escolha_1 para escolha_2
+        escolha_2 = str(input("A. Editar Aluno / B. Editar Turma / C. Editar Grupos de Alunos / D. Editar Notas: ")).strip().lower()
+        if escolha_2 == 'a':  
             editar_aluno.editar_aluno()
-        elif escolha_2 == 'b':  # Corrigido de escolha_1 para escolha_2
+            
+        elif escolha_2 == 'b':  
             editar_turmas.editar_turmas()
 
+        #elif escolha_2 == 'c'
+        #elif escolha_2 == 'd'
+
     elif opcao == "3":
-        print("A. Relatório Json das turmas")  # Removido o .strip().lower()
-        print("B. Relatório Json dos ciclos das notas")  # Removido o .strip().lower()
+        print("A. Relatório Json das turmas") 
+        print("B. Relatório Json dos ciclos das notas") 
         escolha_relatorio = str(input("Qual relatório deseja ver? ")).strip().lower()
         if escolha_relatorio == 'a':
-            cadastro_turma.relatorio_turmas()
+            cadastrar_aluno.relatorio_turmas()
         elif escolha_relatorio == 'b':
             aluno_ciclo.relatorio_ciclos_notas()
 
