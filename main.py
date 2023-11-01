4ad823f1f47c8adb97d8896c388f86f1dda777c4
Yan Yamim <yanyamim@gmail.com>
2023-11-01 13:23:42 -0300

    API

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import editar_aluno
 
 # Função para exibir opções de menu
 def exibir_menu():
-    print("O que você quer fazer hoje?")
-    print("1. Cadastros")
-    #print("2. Editar")
-    print("3. Relatório")
-    print("4. Sair")
+    print("Onde você quer ir hoje?")
+    print("1. Alunos")
+    print("2. Turmas")
+    print("3. Grupos de Alunos")
+    print("4. Ciclos e Notas")
+    print("5. Sair")
 
 # Loop do menu
 while True:
@@ -17,37 +18,33 @@ while True:
     opcao = input("Opção: ")
 
     if opcao == "1":
-        escolha_1 = str(input("A. Cadastro de aluno / B. Cadastro de Turmas / C. Grupos de Aluno / D. Ciclos/Notas: ")).strip().lower()
+        escolha_1 = str(input("A. Cadastro de aluno / B. Editar Aluno")).strip().lower()
         if escolha_1 == 'a':
             cadastrar_aluno.cadastrar_alunos()
 
-        #elif escolha_1 == 'b'
-        #elif escolha_1 == 'c'
+        else:
+            editar_aluno.editar_aluno()
 
-        elif escolha_1 == 'd':
-            aluno_ciclo.registrar_ciclos_e_notas()
-
-    #elif opcao == "2":
-        #escolha_2 = str(input("A. Editar Aluno / B. Editar Turma / C. Editar Grupos de Alunos / D. Editar Notas: ")).strip().lower()
-       # if escolha_2 == 'a':  
-           # editar_aluno.editar_aluno()
-            
-       # elif escolha_2 == 'b':  
-            #editar_turmas.editar_turmas()
-
-        #elif escolha_2 == 'c'
-        #elif escolha_2 == 'd'
+    elif opcao == "2":
+        escolha_2 = str(input("A. Cadastrar Turma / B. Editar Turma")).strip().lower()
+        if escolha_2 == 'a':  
+           cadastrar_aluno.cadastrar_alunos()
+        else:
+            editar_turmas.editar_turmas() 
 
     elif opcao == "3":
-        print("A. Relatório Json das turmas") 
-        print("B. Relatório Json dos ciclos das notas") 
-        escolha_relatorio = str(input("Qual relatório deseja ver? ")).strip().lower()
-        if escolha_relatorio == 'a':
-            cadastrar_aluno.relatorio_turmas()
-        elif escolha_relatorio == 'b':
-            aluno_ciclo.relatorio_ciclos_notas()
+        escolha_3 = str(input("A. Cadastrar Grupo / B. Editar Grupo")).strip().lower()
+        if escolha_3 == 'a':
+            pass
 
     elif opcao == "4":
+        escolha_4 = str(input("A. Criar Ciclo/Nota / B. Editar Ciclo/Notas")).strip().lower()
+        if escolha_4 == 'a':
+            aluno_ciclo.registrar_ciclos_e_notas()
+        else:
+            pass
+
+    elif opcao == "5":
         break
     else:
         print("Opção inválida. Tente novamente.")
