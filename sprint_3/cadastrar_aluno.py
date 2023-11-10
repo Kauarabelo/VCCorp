c6c21b50a1dd0747fd019c2b0f12fe3b838787f2
YanYamim <yanyamim@gmail.com>
2023-11-10 14:34:16 -0300

    Sprint 4

--- sprint_3/cadastrar_aluno.py
+++ sprint_3/cadastrar_aluno.py
@@ -3,7 +3,7 @@ from editar_alunos import editar_aluno
 
 def carregar_dados_alunos():
     try:
-        with open('dados.json', '') as arquivo_dados_alunos_json:
+        with open('dados.json', 'r') as arquivo_dados_alunos_json:
             dados_alunos = json.load(arquivo_dados_alunos_json)
     except FileNotFoundError:
         # Se o arquivo não existir, cria uma estrutura vazia
@@ -26,63 +26,65 @@ def func_cadastrar_alunos():
     while True:
         ra_aluno = input('Informe o RA do aluno a ser cadastrado: ')
 
-        
         while True:
             nome_aluno = input('Qual nome do aluno a ser cadastrado? ')
-            if nome_aluno.isalpha() and len(nome_aluno)<=70:
+            if nome_aluno.isalpha() and len(nome_aluno) <= 70:
                 break
             else:
                 print('Nome inválido, escreva apenas com letras')
-        while True:        
+
+        while True:
             idade_aluno = input('Qual idade do aluno a ser cadastrado? ')
-            if idade_aluno.isdigit() and int(idade_aluno) <=130:
+            if idade_aluno.isdigit() and int(idade_aluno) <= 130:
                 break
             else:
                 print('Idade inválida, escreva apenas com números')
-        while True:    
+
+        while True:
             email_aluno = input('Qual e-mail do aluno a ser cadastrado? ')
             
-            if '@' and '.'in email_aluno:
+            if '@' and '.' in email_aluno:
                 break
             else:
                 print('E-mail inválido, escreva novamente')
-            novo_aluno = {
-                'RA': ra_aluno,
-                'Nome': nome_aluno,
-                'Idade': idade_aluno,
-                'E-mail': email_aluno,
-                'turmas': [],
-                'grupos': []
-            }
+        
+        novo_aluno = {
+            'RA': ra_aluno,
+            'Nome': nome_aluno,
+            'Idade': idade_aluno,
+            'E-mail': email_aluno,
+            'turmas': [],
+            'grupos': []
+        }
 
-            while True:
-                # Exibir dados do aluno para confirmação e opções
-                print(f'\n\n\nRA: {ra_aluno}')
-                print(f'Nome: {nome_aluno}')
-                print(f'Idade: {idade_aluno}')
-                print(f'E-mail: {email_aluno}')
+        while True:
+            # Exibir dados do aluno para confirmação e opções
+            print(f'\n\n\nRA: {ra_aluno}')
+            print(f'Nome: {nome_aluno}')
+            print(f'Idade: {idade_aluno}')
+            print(f'E-mail: {email_aluno}')
 
-                menu_cad_aluno_1 = input('Para confirmar, escolha a opção:\n1-Alterar Nome\n2-Alterar Idade\n3-Alterar E-mail\n4-Salvar e cadastrar outro aluno\n5-Confirmar cadastro e voltar ao menu\n6-Cancelar cadastro e voltar ao menu\n')
+            menu_cad_aluno_1 = input('Para confirmar, escolha a opção:\n1-Alterar Nome\n2-Alterar Idade\n3-Alterar E-mail\n4-Salvar e cadastrar outro aluno\n5-Confirmar cadastro e voltar ao menu\n6-Cancelar cadastro e voltar ao menu\n')
 
-                if menu_cad_aluno_1 == '1':
-                    nome_aluno = input('Novo Nome: ')
-                elif menu_cad_aluno_1 == '2':
-                    idade_aluno = input('Nova Idade: ')
-                elif menu_cad_aluno_1 == '3':
-                    email_aluno = input('Novo E-mail: ')
-                elif menu_cad_aluno_1 == '4':
-                    # Salva os dados do aluno atual e permite cadastrar outro aluno
-                    dados_alunos['alunos'][ra_aluno] = novo_aluno
-                    with open('dados.json', 'w') as arquivo_dados_alunos_json:
-                        json.dump(dados_alunos, arquivo_dados_alunos_json, indent=4)
-                    print('Cadastro realizado com sucesso. Continuando com o próximo aluno.')
-                    break  # Sai do loop interno e permite cadastrar outro aluno
-                elif menu_cad_aluno_1 == '5':
-                    dados_alunos['alunos'][ra_aluno] = novo_aluno
-                    with open('dados.json', 'w') as arquivo_dados_alunos_json:
-                        json.dump(dados_alunos, arquivo_dados_alunos_json, indent=4)
-                    print('Cadastro realizado com sucesso.')
-                    return True
-                elif menu_cad_aluno_1 == '6':
-                    print('Cadastro cancelado.')
-                    return False
+            if menu_cad_aluno_1 == '1':
+                nome_aluno = input('Novo Nome: ')
+            elif menu_cad_aluno_1 == '2':
+                idade_aluno = input('Nova Idade: ')
+            elif menu_cad_aluno_1 == '3':
+                email_aluno = input('Novo E-mail: ')
+            elif menu_cad_aluno_1 == '4':
+                # Salva os dados do aluno atual e permite cadastrar outro aluno
+                dados_alunos['alunos'][ra_aluno] = novo_aluno
+                with open('dados.json', 'w') as arquivo_dados_alunos_json:
+                    json.dump(dados_alunos, arquivo_dados_alunos_json, indent=4)
+                print('Cadastro realizado com sucesso. Continuando com o próximo aluno.')
+                break  # Sai do loop interno e permite cadastrar outro aluno
+            elif menu_cad_aluno_1 == '5':
+                dados_alunos['alunos'][ra_aluno] = novo_aluno
+                with open('dados.json', 'w') as arquivo_dados_alunos_json:
+                    json.dump(dados_alunos, arquivo_dados_alunos_json, indent=4)
+                print('Cadastro realizado com sucesso.')
+                return True
+            elif menu_cad_aluno_1 == '6':
+                print('Cadastro cancelado.')
+                return False
